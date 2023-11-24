ca9e841f71b04df3f66af69bd13f68c7cc077ea1
Botond Zih <zihbot@gmail.com>
2023-11-24 11:02:37 +0100

    debug start

--- scenes/main/Main.cs
+++ scenes/main/Main.cs
@@ -6,6 +6,11 @@ public partial class Main : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		if (OS.IsDebugBuild())
+		{
+			var macroMap = ResourceLoader.Load<PackedScene>("res://scenes/macro/MacroMap.tscn");
+			AddChild(macroMap.Instantiate());
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
