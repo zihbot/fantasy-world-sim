6e49c22bd6ad4035ac7c7ce72f3dd4fb5e38709f
Botond Zih <zihbot@gmail.com>
2023-11-27 20:17:40 +0100

    Refactor _Draw method in MacroMap.cs

--- scenes/macro/MacroMap.cs
+++ scenes/macro/MacroMap.cs
@@ -8,13 +8,13 @@ public partial class MacroMap : Control
 
 	}
 
-    public override void _Draw()
-    {
-        DrawPolygon(new Vector2[] {
+	public override void _Draw()
+	{
+		DrawPolygon(new Vector2[] {
 			new(10, 0),
 			new(10, Size.Y),
 			new(Size.X, Size.Y),
 			new(Size.X, 0),
-		}, new Color[]{new(0, 0, 0, 0.5f)});
-    }
+		}, new Color[] { new(0, 0, 0, 0.5f) });
+	}
 }
