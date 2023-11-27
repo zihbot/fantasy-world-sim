c83d361f48b084d6b57fb0f5f344c850c0cbed37
Botond Zih <zihbot@gmail.com>
2023-11-27 20:17:26 +0100

    draw

---
+++ scenes/macro/MacroMap.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class MacroMap : Control
+{
+	public override void _Ready()
+	{
+
+	}
+
+    public override void _Draw()
+    {
+        DrawPolygon(new Vector2[] {
+			new(10, 0),
+			new(10, Size.Y),
+			new(Size.X, Size.Y),
+			new(Size.X, 0),
+		}, new Color[]{new(0, 0, 0, 0.5f)});
+    }
+}
